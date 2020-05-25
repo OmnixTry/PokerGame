e5fbf1c91c5f9a18f1fb3fa923f3073b9640ac3b
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2020-05-25 19:08:03 +0300

    Cards class added

---
+++ Cards/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGameProject.Cards
+{
+    public enum SUIT
+    {
+        HEARTS,
+        SPADES,
+        DIAMONDS,
+        CLUBS   
+    }
+
+    public enum VALUE
+    {
+        TWO = 2, THREE, FOUR, FIVE, SIX, SEVEN,
+        EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
+    class Card
+    {
+        private readonly SUIT suit;
+        private readonly VALUE value;
+
+        public SUIT Suit { get { return suit; } }
+        public VALUE Value { get { return value; } }
+
+        public Card(SUIT st, VALUE val)
+        {
+            suit = st;
+            value = val;
+        }
+    }
+}
