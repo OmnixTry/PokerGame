5d03613df537bf72da51e0245f409c98a7c3f46d
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2020-05-27 19:45:50 +0300

    Interface for combination checkers added(ICombinationChecker)

---
+++ CardEvaluation/ICombinationChecker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGameProject
+{
+    interface ICombinationChecker
+    {
+        void SetNext(ICombinationChecker nextHandler);
+        HandValue CheckCombination();
+    }
+}
