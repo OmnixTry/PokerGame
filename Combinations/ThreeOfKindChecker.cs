1ea852ac28ef7ec0da4494b3a2ac530c2a6d1e82
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2020-05-28 17:12:17 +0300

    threeOfKindChecker added and tested

---
+++ Combinations/ThreeOfKindChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGameProject.Combinations
+{
+    class ThreeOfKindChecker : BaseCombinationChecker
+    {
+        public override void CheckCombination(HandValue handValue)
+        {
+            var groupsByValue = handValue.Cards.GroupBy(c => c.Value).OrderByDescending(g => g.Key);
+            foreach(var group in groupsByValue)
+            {
+                if(group.Count() == 3)
+                {
+                    handValue.Total = group.Select(x => (int)x.Value).Sum();
+                    handValue.HighCard = (int)handValue.Cards.Except(group).OrderBy(x => x.Value).First().Value;
+                    handValue.Combination = COMBINATION.ThreeKind;
+                    return;
+                }
+            }
+
+        }
+    }
+}
