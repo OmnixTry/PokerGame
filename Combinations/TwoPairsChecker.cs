605075e08eb1f35821a562a644b6e73734167c53
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2020-05-28 18:59:11 +0300

    two pairs checker added

---
+++ Combinations/TwoPairsChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGameProject.Combinations
+{
+    class TwoPairsChecker : BaseCombinationChecker
+    {
+        public override void CheckCombination(HandValue handValue)
+        {
+            var groups = handValue.Cards
+                .GroupBy(c => c.Value)
+                .Where(g => g.Count() == 2)
+                .OrderByDescending(g => g.Key);
+
+            if(groups.Count() >= 2)
+            {
+                foreach (var group in groups.Take(2))
+                {
+                    handValue.Total = group.Sum(c => (int)c.Value);
+                }
+                handValue.HighCard = (int)handValue.Cards
+                    .GroupBy(c => c.Value)
+                    .OrderByDescending(g => g.Key).Skip(2).First().Key;
+                handValue.Combination = COMBINATION.TwoPairs;
+                return; 
+            }
+
+            if (_nextChecker != null)
+            {
+                _nextChecker.CheckCombination(handValue);
+            }
+        }
+    }
+}
