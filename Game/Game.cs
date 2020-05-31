c282e5540331c425f1b740fdbe8ccc02f0750e81
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2020-05-31 13:17:19 +0300

    game class added to project

---
+++ Game/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGameProject.Game
+{
+    class Game
+    {
+        private IGameStrategy _strategy;
+
+        public Game(IGameStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void SetStrategy(IGameStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void PlayGame(uint startMoney)
+        {
+            _strategy.Play(startMoney);
+        }
+    }
+}
