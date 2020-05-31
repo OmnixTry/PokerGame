512ea3b5b87e7bce354724ec9fc924d049625f0e
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2020-05-31 13:12:40 +0300

    Interface for game class added

---
+++ Game/IGameStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGameProject.Game
+{
+    interface IGameStrategy
+    {
+        void Play(uint startMoney);
+    }
+}
